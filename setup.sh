2535eed0c16fb27e221b2118919588d12084291a
JRV-XVI <jrv.software@outlook.com>
2025-07-16 14:46:29 -0600

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# Copying to right places
+xdg-user-dirs-update
+cp alacritty/.alacritty.toml ~/.config/
+cp picom/picom.conf ~/.config/
+cp -r qtile/ ~/.config/
+cp -r rofi/ ~/.config/
+cp -r neovim/ ~/.config/
+cp -r ranger/ ~/.config/
+cp starship/starship.toml ~/.config/
+cp zsh/.zshrc ~/
+cp -r wallpapers/ ~/Pictures/
